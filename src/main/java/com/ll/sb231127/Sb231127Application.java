32fdc446d7c926363257c4e8a023f9b384f19a9b
silvercho <tjddms01212@gmail.com>
2023-11-28 11:35:05 +0900

    setting

---
+++ src/main/java/com/ll/sb231127/Sb231127Application.java
@@ -0,0 +1,13 @@
+package com.ll.sb231127;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb231127Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb231127Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb231127/Sb231127ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb231127;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb231127ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
