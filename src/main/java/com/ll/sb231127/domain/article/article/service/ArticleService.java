d16ef6da272b648fae44bfe7de19f1873c778f58
silvercho <tjddms01212@gmail.com>
2023-11-28 16:18:28 +0900

    articleService

---
+++ src/main/java/com/ll/sb231127/domain/article/article/service/ArticleService.java
@@ -0,0 +1,27 @@
+package com.ll.sb231127.domain.article.article.service;
+
+import com.ll.sb231127.domain.article.article.entity.Article;
+import com.ll.sb231127.domain.article.article.repository.ArticleRepository;
+import com.ll.sb231127.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    @Transactional
+    public RsData<Article> write(String title, String body) {
+        Article article = Article.builder()
+                .title(title)
+                .body(body)
+                .build();
+
+        articleRepository.save(article);
+
+        return RsData.of("200", "%d번 게시글이 작성되었습니다.".formatted(article.getId()), article);
+    }
+}
---
+++ src/test/java/com/ll/sb231127/domain/article/article/service/ArticleServiceTest.java
@@ -0,0 +1,29 @@
+package com.ll.sb231127.domain.article.article.service;
+
+import com.ll.sb231127.domain.article.article.entity.Article;
+import com.ll.sb231127.global.rsData.RsData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class ArticleServiceTest {
+    @Autowired
+    private ArticleService articleService;
+
+    @DisplayName("글 쓰기")
+    @Test
+    void t1() {
+        RsData<Article> writeRs = articleService.write("제목","내용");
+        Article article = writeRs.getData();
+
+        assertThat(article.getId()).isGreaterThan(0L);
+    }
+}
