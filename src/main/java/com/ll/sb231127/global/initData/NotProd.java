7fd913e27e32408fc0615fc5a8d4230b8d3dc032
silvercho <tjddms01212@gmail.com>
2023-11-29 10:53:31 +0900

    Transactional

--- src/main/java/com/ll/sb231127/global/initData/NotProd.java
+++ src/main/java/com/ll/sb231127/global/initData/NotProd.java
@@ -4,10 +4,12 @@ import com.ll.sb231127.domain.article.article.entity.Article;
 import com.ll.sb231127.domain.article.article.service.ArticleService;
 import com.ll.sb231127.domain.member.member.entity.Member;
 import com.ll.sb231127.domain.member.member.service.MemberService;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
 
 @Profile("!prod")
 @Configuration
@@ -17,18 +19,22 @@ public class NotProd {
             MemberService memberService,
             ArticleService articleService
     ){
-        return args -> {
-           Member member1 = memberService.join("user1", "1234").getData();
-           Member member2 = memberService.join("user2", "1234").getData();
+        return new ApplicationRunner() {
+            @Transactional
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Member member1 = memberService.join("user1", "1234").getData();
+                Member member2 = memberService.join("user2", "1234").getData();
 
-            Article article1 = articleService.write(member1.getId(), "제목1", "내용1").getData();
-            Article article2 = articleService.write(member1.getId(), "제목2", "내용2").getData();
+                Article article1 = articleService.write(member1.getId(), "제목1", "내용1").getData();
+                Article article2 = articleService.write(member1.getId(), "제목2", "내용2").getData();
 
-            Article article3 = articleService.write(member2.getId(), "제목3", "내용3").getData();
-            Article article4 = articleService.write(member2.getId(), "제목4", "내용4").getData();
+                Article article3 = articleService.write(member2.getId(), "제목3", "내용3").getData();
+                Article article4 = articleService.write(member2.getId(), "제목4", "내용4").getData();
 
-            article1.addComment(member2 , "댓글1");
-            article1.addComment(member2, "댓글2");
+                article1.addComment(member1, "댓글1");
+                article1.addComment(member1, "댓글1");
+            }
         };
     }
 }
