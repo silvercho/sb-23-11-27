717029a171b327c38d16c72776aa6314e7285fd1
silvercho <tjddms01212@gmail.com>
2023-11-29 13:44:35 +0900

    t7

--- src/test/java/com/ll/sb231127/domain/article/article/service/ArticleServiceTest.java
+++ src/test/java/com/ll/sb231127/domain/article/article/service/ArticleServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,9 +61,19 @@ public class ArticleServiceTest {
 
         assertThat(article_.getTitle()).isEqualTo("수정된 제목");
     }
+    @DisplayName("2번글에 댓글들을 추가한다.")
+    @Test
+    @Rollback(false)
+    void t5() {
+        Member member1 = memberService.findById(1L).get();
+        Article article2 = articleService.findById(2L).get();
+
+        article2.addComment(member1, "댓글 입니다.");
+    }
+
     @DisplayName("1번 글의 댓글들을 수정한다.")
     @Test
-    void t5() {
+    void t6() {
         Article article = articleService.findById(1L).get();
 
         article.getComments().forEach(comment -> {
@@ -71,7 +82,7 @@ public class ArticleServiceTest {
     }
     @DisplayName("1번 글의 댓글 중 마지막 것을 삭제한다.")
     @Test
-    void t6() {
+    void t7() {
         Article article = articleService.findById(1L).get();
 
         ArticleComment lastComment = article.getComments().getLast();
