8fc0389527bbb449b3b4a91dc8f261d4271d8a98
silvercho <tjddms01212@gmail.com>
2023-11-30 10:44:25 +0900

    38

--- src/main/java/com/ll/sb231127/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/sb231127/domain/member/member/service/MemberService.java
@@ -32,4 +32,8 @@ public class MemberService {
     public Optional<Member> findById(long id){
         return memberRepository.findById(id);
     }
+
+    public long count() {
+        return memberRepository.count();
+    }
 }
