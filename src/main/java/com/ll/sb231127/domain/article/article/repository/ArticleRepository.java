ca81c4f0dc2139d6574a01efb6478bc4ff4b6d27
silvercho <tjddms01212@gmail.com>
2023-11-30 11:45:07 +0900

    40

--- src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
@@ -9,10 +9,12 @@ import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByOrderByIdDesc();
+    Page<Article> findByAuthor_usernameContainingOrTitleContainingOrBodyContainingOrTags_contentOrComments_author_usernameContainingOrComments_bodyContaining(String kw, String kw_, String kw__, String kw___, String kw____, String kw_____, Pageable pageable);
+    Page<Article> findByAuthor_usernameContainingOrTitleContainingOrBodyContaining(String kw, String kw_, String kw__, Pageable pageable);
 
     Page<Article> findByTitleContainingOrBodyContaining(String kw , String kw_, Pageable pageable);
     Page<Article> findByTitleContaining(String kw, Pageable pageable);
 
     Page<Article> findByBodyContaining(String kw, Pageable pageable);
-
+    Page<Article> findByAuthor_usernameContaining(String kw, Pageable pageable);
 }
