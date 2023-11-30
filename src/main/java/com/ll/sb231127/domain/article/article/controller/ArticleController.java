5d2f226ffa3eac678093ddbcb80b8cead3efa48a
silvercho <tjddms01212@gmail.com>
2023-11-30 11:20:38 +0900

    JPA

--- src/main/java/com/ll/sb231127/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb231127/domain/article/article/controller/ArticleController.java
@@ -30,6 +30,7 @@ public class ArticleController {
     public String list(
             @RequestParam(value = "kwType" , defaultValue = "")
                     List<String> kwTypes,
+            @RequestParam(defaultValue = "") String kw,
             @RequestParam(defaultValue = "0") int page,
             Model model
     ) {
@@ -44,7 +45,7 @@ public class ArticleController {
                         kwType -> true
                 ));
 
-        Page<Article> itemsPage = articleService.search(pageable);
+        Page<Article> itemsPage = articleService.search(kwTypes, kw, pageable);
         model.addAttribute("itemsPage" , itemsPage);
         model.addAttribute("kwTypesMap", kwTypesMap);
 
--- src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
@@ -1,11 +1,18 @@
 package com.ll.sb231127.domain.article.article.repository;
 
 import com.ll.sb231127.domain.article.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByOrderByIdDesc();
+
+    Page<Article> findByTitleContainingOrBodyContaining(String kw , String kw_, Pageable pageable);
+    Page<Article> findByTitleContaining(String kw, Pageable pageable);
+
+    Page<Article> findByBodyContaining(String kw, Pageable pageable);
 
 }
