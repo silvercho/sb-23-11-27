46d5a89fe0d329d9815eaef1de2adca03276f6aa
silvercho <tjddms01212@gmail.com>
2023-11-30 10:46:22 +0900

    table

--- src/main/java/com/ll/sb231127/global/initData/NotProd.java
+++ src/main/java/com/ll/sb231127/global/initData/NotProd.java
@@ -1,4 +1,5 @@
 package com.ll.sb231127.global.initData;
+
 import com.ll.sb231127.domain.article.article.entity.Article;
 import com.ll.sb231127.domain.article.article.service.ArticleService;
 import com.ll.sb231127.domain.member.member.entity.Member;
@@ -11,6 +12,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.IntStream;
+
 @Profile("!prod")
 @Configuration
 @RequiredArgsConstructor
@@ -32,6 +36,8 @@ public class NotProd {
 
         Member member1 = memberService.join("user1", "1234").getData();
         Member member2 = memberService.join("user2", "1234").getData();
+        Member member3 = memberService.join("user3", "1234").getData();
+
         Article article1 = articleService.write(member1.getId(), "제목1", "내용1").getData();
         Article article2 = articleService.write(member1.getId(), "제목2", "내용2").getData();
         Article article3 = articleService.write(member2.getId(), "제목3", "내용3").getData();
@@ -44,8 +50,27 @@ public class NotProd {
         article2.addComment(member1, "댓글4");
         article2.addComment(member1, "댓글5");
 
+        article3.addComment(member1, "댓글5");
+        article3.addComment(member1, "댓글6");
+        article3.addComment(member1, "댓글7");
+        article3.addComment(member1, "댓글8");
+        article3.addComment(member1, "댓글9");
+        article3.addComment(member1, "댓글10");
+        article3.addComment(member1, "댓글11");
+        article3.addComment(member1, "댓글12");
+
         article1.addTag("자바");
         article1.addTag("백엔드");
         article2.addTag("프레임워크","스프링부트");
+
+        article4.addTag("자바", "스프링부트");
+
+        IntStream.rangeClosed(5, 120).forEach(
+                i -> {
+                    String title = "제목" + i;
+                    String body = "내용" + i;
+                    articleService.write(member2.getId(), title, body);
+                }
+        );
     }
 }
