c1dfdaa2b4cae7d0de3006d413b6d2057c5cc346
silvercho <tjddms01212@gmail.com>
2023-11-30 10:51:36 +0900

    39강

--- src/main/java/com/ll/sb231127/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb231127/domain/article/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.ll.sb231127.domain.article.article.controller;
 import com.ll.sb231127.domain.article.article.entity.Article;
 import com.ll.sb231127.domain.article.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,21 +16,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/article")
 @RequiredArgsConstructor
+@Slf4j // 로깅 인터베이스
 public class ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("/list")
     public String list(
-            @RequestParam(value = "page" , defaultValue = "0") int page,
+            @RequestParam(value = "kwType" , defaultValue = "")
+                    List<String> kwTypes,
+            @RequestParam(defaultValue = "0") int page,
             Model model
     ) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("id"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+
+        Map<String, Boolean> kwTypesMap = kwTypes
+                .stream()
+                .collect(Collectors.toMap(
+                        kwType -> kwType,
+                        kwType -> true
+                ));
+
+        log.debug("kwTypesMap: {}", kwTypesMap);
 
         Page<Article> itemsPage = articleService.search(pageable);
         model.addAttribute("itemsPage" , itemsPage);
