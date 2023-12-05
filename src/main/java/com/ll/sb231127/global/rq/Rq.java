2f6aef150a0c6c3111d604b5a76c9b6dc7773256
silvercho <tjddms01212@gmail.com>
2023-12-05 09:40:25 +0900

    rq.get~~

---
+++ src/main/java/com/ll/sb231127/global/rq/Rq.java
@@ -0,0 +1,27 @@
+package com.ll.sb231127.global.rq;
+
+import com.ll.sb231127.standard.util.Ut;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@RequestScope
+@Component
+@RequiredArgsConstructor
+public class Rq {
+    private final HttpServletRequest req;
+    private final HttpServletResponse resp;
+
+    public String getCurrentQueryStringWithoutParam(String paramName) {
+        String queryString = req.getQueryString();
+
+        if (queryString == null) {
+            return "";
+        }
+        queryString = Ut.url.deleteQueryParam(queryString, paramName);
+
+        return queryString;
+    }
+}
